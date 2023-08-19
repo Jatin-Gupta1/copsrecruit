47f89d110a6b89fdaf8cca77d58199f4a12cae05
jatin <jatn.gupta.cd.mat22@itbhu.ac.in>
2023-08-19 18:44:59 +0530

    added navbar and page1

--- main.js
+++ main.js
@@ -4,3 +4,32 @@ main.addEventListener('mousemove',function(dets){
     crsr.style.left=dets.x+"px";
     crsr.style.top=dets.y+"px";
 })
+
+var tl=gsap.timeline({
+    scrollTrigger:{
+        trigger:".page1 h1",
+        scroller:'#main',
+        start:'top 27%',
+        end:'top 0%',
+        
+        scrub:3
+    }
+})
+tl.to('.page1 h1',{
+    x:-100,  
+},"anim")
+tl.to(".page1 h2",{
+    x:100
+},"anim")
+
+gsap.to(".image",{
+    y:"-105%",
+    width:"100%",
+    scrollTrigger:{
+        trigger:".page1",
+        scroller:'#main',
+        pin:true,
+        start:"top 0%",
+        scrub:true
+    }
+},"anim")
