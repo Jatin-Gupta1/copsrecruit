ec1d7b6c58f41f00941887f8fb3ad582c5b117aa
jatin <jatn.gupta.cd.mat22@itbhu.ac.in>
2023-08-19 18:57:19 +0530

    Added gsap and locomotive js

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
+function init(){
+    gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, 
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
+}
+init();
+
 var crsr=document.querySelector(".cursor");
 var main=document.querySelector('#main');
 main.addEventListener('mousemove',function(dets){
@@ -11,7 +40,6 @@ var tl=gsap.timeline({
         scroller:'#main',
         start:'top 27%',
         end:'top 0%',
-        
         scrub:3
     }
 })
